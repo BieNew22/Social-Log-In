76cf92cca0cc7b0f64634691ce366c0c6459eb68
BieNew22 <newbie11004@gmail.com>
2024-09-03 22:19:53 +0900

    Feat: add routing, change start link to /auth/login

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        // 기본 접속시 -> home으로 리다이렉트
+        path: '',
+        redirectTo: 'auth/login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.routes').then(m => m.AuthRoutingModule)
+    }
+];
+
+// more information : https://www.angular.kr/guide/router
---
+++ front-end/src/app/auth/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+
+@Component({
+    selector: 'app-auth-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+    ngOnInit(): void {
+        
+    }
+}

ef161a2e75cc5b460dfc74893d035eb247d6bfbb
BieNew22 <newbie11004@gmail.com>
2024-09-04 12:48:55 +0900

    Feat: add secret.component and auth.guard

---
+++ front-end/src/app/secret/secret.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+
+@Component({
+    selector: 'app-secret-main',
+    templateUrl: './secret.component.html',
+    styleUrls: ['./secret.component.scss']
+})
+export class SecretComponent implements OnInit {
+    ngOnInit(): void {
+        
+    }
+}

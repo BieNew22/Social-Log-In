cabbf323d96b0b1d51f8b755c50eedb6787ce64e
BieNew22 <newbie11004@gmail.com>
2024-09-05 09:27:59 +0900

    Feat: add Home page, remove login page's secret button

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -2,13 +2,19 @@ import { Routes } from '@angular/router';
 import { AuthGuard } from './auth/core/auth.guard';
 
 import { SecretComponent } from './secret/secret.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     {
         // 기본 접속시 -> home으로 리다이렉트
         path: '',
-        redirectTo: 'auth/login',
+        redirectTo: '/home',
         pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent
     },
     {
         path: 'auth',
